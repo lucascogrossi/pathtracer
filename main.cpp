23ff0164adf45ad9251b7d7f791c9d0c149a4406
lucascogrossi <contact@lucascogrossi.com>
2025-12-03 19:39:45 -0300

    add progress indicator

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@ int main() {
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for (int j = 0; j < image_height; j++) {
+        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
         for (int i = 0; i < image_width; i++) {
 
             // Create a gradient from 0.0 to 1.0
@@ -25,4 +26,5 @@ int main() {
             std::cout << ir << ' ' << ig << ' ' << ib << '\n';
         }
     }
+    std::clog << "\rDone.                 \n";
 }
