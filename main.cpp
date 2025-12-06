7d6f08ceb914d60eaa7a731ecb07719c6aa9e348
lucascogrossi <contact@lucascogrossi.com>
2025-12-06 17:16:08 -0300

    red sphere

--- main.cpp
+++ main.cpp
@@ -4,8 +4,21 @@
 
 #include <iostream>
 
+bool hit_sphere(const point3& center, double radius, const ray& r) {
+    vec3 oc = center - r.origin();
+    auto a = dot(r.direction(), r.direction());
+    auto b = -2.0 * dot(r.direction(), oc);
+    auto c = dot(oc, oc) - radius * radius;
+    auto discriminant = b*b - 4 * a * c;
+    return (discriminant >= 0);
+}
+
 color ray_color(const ray& r) {
-    // Rendering a blue-to-white gradient
+    // Render red sphere
+    if (hit_sphere(point3(0,0,-1), 0.5, r))
+        return color(1, 0, 0);
+
+    // Rendering a blue-to-white gradient background
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5 * (unit_direction.y() + 1.0);
     return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
@@ -24,7 +37,7 @@ int main() {
     // Camera
     auto focal_lenght = 1.0;
     auto viewport_height = 2.0;
-    auto viewport_width = viewport_height * double((image_width) / image_height);
+    auto viewport_width = viewport_height * (double(image_width) / image_height);
     auto camera_center = point3(0, 0, 0);
 
     // Calculate the vectors across the horizontal and down the vertical vieweport edges
