4116997c3c6fd765441a0a61543b58b9f93e0ffc
lucascogrossi <contact@lucascogrossi.com>
2025-12-07 19:45:15 -0300

    hittable.hpp & sphere.hpp

---
+++ hittable.hpp
@@ -0,0 +1,29 @@
+#ifndef HITTABLE_H
+#define HITTABLE_H
+
+#include "ray.hpp"
+
+class hit_record {
+    public:
+        point3 p;
+        vec3 normal;
+        double t;
+        bool front_face;
+
+        void set_face_normal(const ray& r, const vec3& outward_normal) {
+            // Sets the hit record normal vector
+            // We assume `outward_normal` to have unit lenght
+
+            front_face = dot(r.direction(), outward_normal) < 0;
+            normal = front_face ? outward_normal : -outward_normal;
+        }
+};
+
+class hittable  {
+    public:
+        virtual ~hittable() = default;
+
+        virtual bool hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec) const = 0;
+};
+
+#endif
