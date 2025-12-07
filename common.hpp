eca897e9944d74552a9285a0abb55baf5194079e
lucascogrossi <contact@lucascogrossi.com>
2025-12-07 20:06:36 -0300

    ~6.8 interval class

---
+++ common.hpp
@@ -0,0 +1,27 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <cmath>
+#include <iostream>
+#include <limits>
+#include <memory>
+
+// C++ std usings
+using std::make_shared;
+using std::shared_ptr;
+
+// Constants
+const double infinity = std::numeric_limits<double>::infinity();
+const double pi = 3.1415926535897932385;
+
+// Utility functions
+inline double degrees_to_radians(double degrees) {
+    return degrees * pi / 180.0;
+}
+
+// Common headers
+#include "color.hpp"
+#include "ray.hpp"
+#include "vec3.hpp"
+
+#endif
