09dc64e60d98978596bbeddc686557b7d8ef3c5c
lucascogrossi <contact@lucascogrossi.com>
2025-12-09 02:14:19 -0300

    bug fix

--- include/material.hpp
+++ include/material.hpp
@@ -41,8 +41,8 @@ class metal : public material {
 
         bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered)
         const override {
-            vec3 reflected = reflect(r_in.direction(), rec.normal);
-            scattered = ray(rec.p, reflected);
+            vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
+            scattered = ray(rec.p, reflected + fuzz * random_unit_vector());
             attenuation = albedo;
             return (dot(scattered.direction(), rec.normal) > 0);
         }
